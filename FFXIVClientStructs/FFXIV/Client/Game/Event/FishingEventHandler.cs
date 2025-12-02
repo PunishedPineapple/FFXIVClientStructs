e846cd846705d87e15e38a82d6457eb81b823a93
PunishedPineapple <50609717+PunishedPineapple@users.noreply.github.com>
2025-12-02 16:25:49 -0600

    Cleaning up a bit.

--- FFXIVClientStructs/FFXIV/Client/Game/Event/FishingEventHandler.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/Event/FishingEventHandler.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-
-using InteropGenerator.Runtime.Attributes;
-
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
@@ -18,32 +9,73 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.Event;
 [Inherits<EventHandler>, Inherits<AtkModuleInterface.AtkEventInterface>]
 [StructLayout(LayoutKind.Explicit, Size = 0x290)]
 public unsafe partial struct FishingEventHandler {
-    [FieldOffset(0x220)] public ulong Unk_220; // Have not seen it be anything besides zero.
+    [FieldOffset(0x220)] public byte Unk_220;
     [FieldOffset(0x228)] public FishingState FishingState;
-    [FieldOffset(0x22C)] public float Sawtooth; // 4 Hz inverse sawtooth between one and zero.  Latches upon cast.  No clue what it is for.
-    [FieldOffset(0x230)] public bool AtFishingHole; // Only updates when on FSH.
-    [FieldOffset(0x231)] public bool CanMoochPreviousCatch; // Returns to false when opportunity is gone (i.e., Spareful Hand is used).
-    [FieldOffset(0x232)] public bool CanMooch2PreviousCatch; // Returns to false when opportunity is gone (i.e., 15s elapsed).  Ignores skill cooldown.
+
+    /// <remarks>
+    /// 4 Hz inverse sawtooth between one and zero.  Latches upon cast.  No clue what it is for.
+    /// </remarks>
+    [FieldOffset(0x22C)] public float Unk_22C;
+
+    /// <remarks>
+    /// Only updates when on FSH.
+    /// </remarks>
+    [FieldOffset(0x230)] public bool AtFishingHole;
+
+    /// <remarks>
+    /// Returns to false when opportunity is gone (i.e., Spareful Hand is used).
+    /// </remarks>
+    [FieldOffset(0x231)] public bool CanMoochPreviousCatch;
+
+    /// <remarks>
+    /// Returns to false when opportunity is gone (i.e., 15s elapsed).  Ignores skill cooldown.
+    /// </remarks>
+    [FieldOffset(0x232)] public bool CanMooch2PreviousCatch;
+
     [FieldOffset(0x233)] public bool CanReleasePreviousCatch;
-    [FieldOffset(0x234)] public bool ChangingPosition; // True while in the process of sitting down or standing up.
+
+    /// <remarks>
+    /// True while in the process of sitting down or standing up.
+    /// </remarks>
+    [FieldOffset(0x234)] public bool ChangingPosition;
+
     [FieldOffset(0x235)] public bool CanIdenticalCastPreviousCatch;
     [FieldOffset(0x236)] public bool CanSurfaceSlapPreviousCatch;
-    [FieldOffset(0x237)] public bool Unk_237;    //	Never seen false.  vf57 actively sets it to true, whatever that does.
+    [FieldOffset(0x237)] public bool Unk_237; // Never seen false.  vf57 actively sets it to true, whatever that does.
     [FieldOffset(0x238)] public FishingBaitFlags CurrentCastBaitFlags;
-    [FieldOffset(0x23C)] public sbyte SelectedSwimBaitIndex; // -1 when none selected.
-    [FieldOffset(0x240), FixedSizeArray] internal FixedSizeArray3<uint> _swimBaitItemIDs;
-    [FieldOffset(0x24C)] public uint Unk_24C;
-    [FieldOffset(0x250)] public long MoochOpportunityExpirationTimestamp_mSec; // Unix timestamp in milliseconds for when the current Mooch/Spareful Hand opportunity will cease being available.
-    [FieldOffset(0x258)] public long CatchActionExpirationTimestamp_mSec; // Unix timestamp in milliseconds for when actions like Surface Slap will cease being available.
 
-    // An instance of something with a vtable right next to this event handler's.  Probably 0x30 bytes long, destructor vfunc 2.
-    //[FieldOffset( 0x260 )] public void* pUnk_260;
-    //[FieldOffset( 0x268 )] public ulong Unk_268;
+    /// <summary>
+    /// The index into <see cref="SwimBaitItemIDs"/> of the currently-selected swimbait.
+    /// </summary>
+    /// <remarks>
+    /// This is -1 when no swimbait has been selected.
+    /// </remarks>
+    [FieldOffset(0x23C)] public sbyte SwimBaitIndex;
+
+    /// <summary>
+    /// The item IDs of currently-stored swimbait.
+    /// </summary>
+    [FieldOffset(0x240), FixedSizeArray] internal FixedSizeArray3<uint> _swimBaitItemIDs;
+
+    [FieldOffset(0x24C)] public uint Unk_24C; // Sometimes matches 0x224, but that offset may just be uninitialized padding.
+
+    /// <summary>
+    /// Unix time (in milliseconds) for when the current Mooch/Spareful Hand opportunity will cease being available (if ever).
+    /// </summary>
+    [FieldOffset(0x250)] public long MoochOpportunityExpirationTime;
+
+    /// <summary>
+    /// Unix time (in milliseconds) for when actions like Surface Slap will cease being available.
+    /// </summary>
+    [FieldOffset(0x258)] public long CatchActionExpirationTime;
+
+    // An instance of something that looks like it has a 6-function vtable right before this event handler's vtable.  Probably 0x30 bytes long.
+    //[FieldOffset( 0x260 )] public void* vTablePtr;
+    //[FieldOffset( 0x268 )] public byte Unk_268;
     //[FieldOffset( 0x270 )] public FishingEventHandler* FishingEventHandlerInstance;
     //[FieldOffset( 0x278 )] public ulong Unk_278;
-    //[FieldOffset( 0x280 )] public int Unk_280;
-    //[FieldOffset( 0x284 )] public int Unk_284;
-    //[FieldOffset( 0x288 )] public ulong Unk_288;
+    //[FieldOffset( 0x280 )] public uint Unk_280;
+    //[FieldOffset( 0x284 )] public ulong Unk_284; // Unaligned, but it disassembles as a qword in the constructor, so idk.
 }
 
 [Flags]
@@ -55,18 +87,36 @@ public enum FishingBaitFlags : int {
     Swimbait = 0x20,
 }
 
-public enum FishingState : byte {
+public enum FishingState : int {
     None = 0,
     CastingOut = 1,
-    PullingPoleIn = 2,      //	When fish slips, there is no bite, briefly after reeling in a fish, and when using Rest.
+    /// <remarks>
+    /// Used when fish slips, when there is no bite, briefly after reeling in a fish, and when using Rest.
+    /// </remarks>
+    PullingPoleIn = 2,
     Quitting = 3,
-    PoleReady = 4,          //	The standby "gathering" condition.
+    /// <summary>
+    /// The standby "gathering" condition.
+    /// </summary>
+    PoleReady = 4,
     Bite = 5,
-    Hooking = 6,            //	Includes the subsequent reeling in.
+    /// <remarks>
+    /// Includes the subsequent reeling in.
+    /// </remarks>
+    Hooking = 6,
     ReleasingCatch = 7,
     ConfirmingCollectable = 8,
-    AmbitiousLure = 9,      //	When using the skill.
-    ModestLure = 10,        //	When using the skill.
+    /// <remarks>
+    /// Only during the action's animation.
+    /// </remarks>
+    AmbitiousLure = 9,
+    /// <remarks>
+    /// Only during the action's animation.
+    /// </remarks>
+    ModestLure = 10,
     Unk_11 = 11,
-    LineInWater = 12,		//	Or air, sand, etc.; just when you are actually fishing.
+    /// <remarks>
+    /// Or air, sand, etc.; just when you are actually fishing.
+    /// </remarks>
+    LineInWater = 12,
 }
