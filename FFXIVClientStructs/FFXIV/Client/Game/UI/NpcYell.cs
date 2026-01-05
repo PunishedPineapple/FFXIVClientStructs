1b4fc3ac7a3dec615a64ca996c608b360aa475e4
PunishedPineapple <50609717+PunishedPineapple@users.noreply.github.com>
2026-01-04 19:45:57 -0600

    Cleanup

--- FFXIVClientStructs/FFXIV/Client/Game/UI/NpcYell.cs
+++ FFXIVClientStructs/FFXIV/Client/Game/UI/NpcYell.cs
@@ -8,29 +8,55 @@ namespace FFXIVClientStructs.FFXIV.Client.Game.UI;
 [GenerateInterop]
 [StructLayout(LayoutKind.Explicit, Size = 0x2850)]
 public partial struct NpcYell {
-
-    [FieldOffset(0x48), FixedSizeArray] internal FixedSizeArray32<NpcYellSpeakerInfo> _speakerInfo;
-    [FieldOffset(0x548), FixedSizeArray] internal FixedSizeArray32<NpcYellInfo> _yells;
-    [FieldOffset(0x2848)] private short Unk_2848; // Probably number of unprocessed yells.
+    [FieldOffset(0x48), FixedSizeArray] internal FixedSizeArray32<NpcYellSlotInfo> _yellSlots;
+    [FieldOffset(0x548), FixedSizeArray] internal FixedSizeArray32<NpcYellInfo> _yellInfo;
+    [FieldOffset(0x2848)] private short Unk_2848; // Probably number of unhandled yells.
 
     [StructLayout(LayoutKind.Explicit, Size = 0x28)]
-    public struct NpcYellSpeakerInfo {
-        [FieldOffset(0x0)] public uint EntityId; // Might be a full GameObjectId, uncertain.
+    public struct NpcYellSlotInfo {
+        [FieldOffset(0x0)] public GameObjectId ObjectId;
         [FieldOffset(0xC)] public uint NameId;
-        [FieldOffset(0x10)] private uint Unk_10; // Probably some kind of ID.  Lines up with the YellInfo field at offset 0xE8.
+        [FieldOffset(0x10)] private uint Unk_10; // Probably some kind of ID.  Corresponds to the NpcYellInfo field at offset 0xE8.
+        [FieldOffset(0x20)] private float Unk_20; // Probably a delay.  Has effective frame delta subracted each update.
     }
 
     [StructLayout(LayoutKind.Explicit, Size = 0x118)]
     public struct NpcYellInfo {
         [FieldOffset(0x0)] public uint NpcYellRowId;
-        [FieldOffset(0x8)] public uint EntityId; // Might be a full GameObjectId, uncertain.
+        [FieldOffset(0x8)] public GameObjectId ObjectId;
         [FieldOffset(0x10)] public uint NameId;
         [FieldOffset(0x18)] public Utf8String Name;
         [FieldOffset(0x80)] public Utf8String Message;
-        [FieldOffset(0xE8)] private uint Unk_E8; // Probably some kind of ID.  Lines up with the SpeakerInfo field at offset 0x10.
+        [FieldOffset(0xE8)] private uint Unk_E8; // Probably some kind of ID.  Corresponds to the NpcYellSpeakerInfo field at offset 0x10.
+        [FieldOffset(0x108)] private uint Unk_108; // Controls which UIModule::ShowBattleTalk is called.
+
+        /// <summary>
+        /// How long (in seconds) to display the message as a balloon.
+        /// </summary>
+        /// <remarks>
+        /// Only used if <see cref="OutputType"/> includes <see cref="NpcYellOutputFlags.ShowBalloon"/>.
+        /// </remarks>
         [FieldOffset(0xFC)] public float BalloonTime;
+
+        /// <summary>
+        /// How long (in seconds) to display the message in the BattleTalk addon.
+        /// </summary>
+        /// <remarks>
+        /// Only used if <see cref="OutputType"/> includes <see cref="NpcYellOutputFlags.ShowBattleTalk"/>.
+        /// </remarks>
         [FieldOffset(0x100)] public float BattleTalkTime;
+
+        /// <summary>
+        /// The method(s) used to show the dialogue to the player.
+        /// </summary>
         [FieldOffset(0x104)] public NpcYellOutputFlags OutputType;
+
+        /// <summary>
+        /// The index of the character bone to which to attach the <see cref="NpcYellBalloon"/>.
+        /// </summary>
+        /// <remarks>
+        /// Only used if <see cref="OutputType"/> includes <see cref="NpcYellOutputFlags.ShowBalloon"/>.
+        /// </remarks>
         [FieldOffset(0x10C)] public byte ParentBone;
         [FieldOffset(0x110)] public NpcYellFlags Flags;
     }
@@ -46,11 +72,23 @@ public partial struct NpcYell {
     [Flags]
     public enum NpcYellFlags : byte {
         None = 0,
+        /// <summary>
+        /// Controls the corresponding flag in the <see cref="NpcYellBalloon"/>.
+        /// </summary>
         SkipCloseChecks = 1,
-        SkipRangeCheck = 2, // Also checked when formatting NPC name.
+        /// <summary>
+        /// Controls the corresponding flag in the <see cref="NpcYellBalloon"/>.
+        /// </summary>
+        /// <remarks>
+        /// Also checked when formatting NPC name.
+        /// </remarks>
+        SkipRangeCheck = 2,
         Unk_4 = 4,
         Unk_8 = 8,
-        Unk_10 = 0x10, // This is the default value after initialization.  Unclear what it does.
+        /// <remarks>
+        /// This is the default value after initialization.
+        /// </remarks>
+        Unk_10 = 0x10,
         Unk_20 = 0x20,
         Unk_40 = 0x40,
     }
